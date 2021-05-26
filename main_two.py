2b5a650b94652ce18d51aabbf15956f6c11b32da
Paola Benedetti <paolabenedetti1009@gmail.com>
2021-05-26 15:53:35 +0200

    Update main_two.py
    changed threshold for keypoints model config

--- main_two.py
+++ main_two.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
                     # cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
                     cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml"))
 
-                    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
+                    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
                     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml")
 
                 elif args['get'] == 'mask':
