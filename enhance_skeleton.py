d5d1034d209c450c527aee9cbb11c5570a33aa4a
Paola Benedetti <paolabenedetti1009@gmail.com>
2021-05-28 15:08:25 +0200

    Update enhance_skeleton.py

--- enhance_skeleton.py
+++ enhance_skeleton.py
@@ -1,8 +1,10 @@
 from typing import Mapping
 
 
+import pickle
+
 if __name__ == '__main__':
-    path = './output/joint/detectron/total_file/V_70_DJA.pkl'
+    path = './output/joint/detectron/total_info/V_70_DJA.pkl'
     with open(path, 'rb') as f:
         file = pickle.load(f)
     print(file[0])
